1011a9d06c75793a98e72757f0f089116a672f38
Amarnath A C <33284585+AmarnathAyyananChakkaravarthy@users.noreply.github.com>
2024-08-01 19:59:24 +0530

    Blog Page | Amarnath

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   darkMode: ['class','[data-theme="dark"]'],
+  "skipLibCheck": true,
   theme: {
     extend: {},
   },
